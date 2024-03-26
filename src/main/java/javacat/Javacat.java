f67598fd1b08414a63e73e254062332d536f6c53
Stephen <stephenbrady365@gmail.com>
2024-03-26 20:49:53 +0000

    add all flag options to Options object

--- src/main/java/javacat/Javacat.java
+++ src/main/java/javacat/Javacat.java
@@ -8,8 +8,7 @@ import java.util.*;
 
 public class Javacat {
     public static void main(String[] args) {
-        Options options = new Options();
-        options.addOption("n", "number",false, "number all output lines");
+        Options options = getOptions();
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd;
@@ -18,12 +17,30 @@ public class Javacat {
         try {
             cmd = parser.parse(options, args);
             if (cmd.hasOption("n")) {
-                catOptions.add("number");
+                catOptions.add("n");
+            }
+            if (cmd.hasOption("b")) {
+                catOptions.add("b");
+                catOptions.remove("n");
             }
             cat(args, catOptions);
         } catch (ParseException ex) {
             ex.getMessage();
         }
+    }
+
+    private static Options getOptions() {
+        Options options = new Options();
+        options.addOption("A", "show-all", false, "equivalent to -vET");
+        options.addOption("b", "number-nonblank", false, "number nonempty output lines, overrides -n");
+        options.addOption("e", false, "equivalent to -vE");
+        options.addOption("E", "show-ends", false, "display $ at end of each line");
+        options.addOption("n", "number",false, "number all output lines");
+        options.addOption("s", "squeeze-blank", false, "suppress repeated empty output lines");
+        options.addOption("t", false, "equivalent to -vT");
+        options.addOption("T", "show-tabs", false, "display TAB characters as ^I");
+        options.addOption("v", "show-nonprinting", false, "use ^ and M- notation, except for LFD and TAB");
+        return options;
     }
 
     private static void cat(String[] args, Set<String> catOptions) {
@@ -32,7 +49,7 @@ public class Javacat {
             int lineNumber = 1;
             while (scanner.hasNext()) {
                 String line = scanner.nextLine();
-                if (catOptions.contains("number")) {
+                if (catOptions.contains("n")) {
                     System.out.print(lineNumber);
                     if (!line.isBlank()) {
                         System.out.print(" ");
