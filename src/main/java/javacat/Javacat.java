0993ae78c8c414dd0fbec0e2e72f98155c70665f
Stephen <stephenbrady365@gmail.com>
2024-03-26 20:35:02 +0000

    add -n flag functionality

--- src/main/java/javacat/Javacat.java
+++ src/main/java/javacat/Javacat.java
@@ -1,17 +1,48 @@
 package javacat;
 
+import org.apache.commons.cli.*;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Javacat {
-    public static void main(String[] args) throws FileNotFoundException {
-        String file = args[0];
-        Scanner scanner = new Scanner(new File(file));
-        while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
-        }
+    public static void main(String[] args) {
+        Options options = new Options();
+        options.addOption("n", "number",false, "number all output lines");
 
-        scanner.close();
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd;
+
+        Set<String> catOptions = new HashSet<>();
+        try {
+            cmd = parser.parse(options, args);
+            if (cmd.hasOption("n")) {
+                catOptions.add("number");
+            }
+            cat(args, catOptions);
+        } catch (ParseException ex) {
+            ex.getMessage();
+        }
+    }
+
+    private static void cat(String[] args, Set<String> catOptions) {
+        String file = catOptions.isEmpty() ? args[0] : args[1];
+        try(Scanner scanner = new Scanner(new File(file))) {
+            int lineNumber = 1;
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                if (catOptions.contains("number")) {
+                    System.out.print(lineNumber);
+                    if (!line.isBlank()) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println(line);
+                lineNumber++;
+            }
+        } catch (FileNotFoundException ex) {
+            ex.getMessage();
+        }
     }
 }
--- src/test/java/javacat/JavacatTest.java
+++ src/test/java/javacat/JavacatTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,14 +18,30 @@ class JavacatTest {
     }
 
     @Test
-    void printsHelloWorld() throws FileNotFoundException {
+    void readsFileAndPrintsToStandardOutput() {
         String[] args = new String[] { "src/test/resources/test2.txt" };
         Javacat.main(args);
-        String expectedOutput = "\"I Don't Believe In Failure. It Is Not Failure If You Enjoyed The Process.\"\n" +
-        "\"Do not get elated at any victory, for all such victory is subject to the will of God.\"\n" +
-        "\"Wear gratitude like a cloak and it will feed every corner of your life.\"\n" +
-        "\"If you even dream of beating me you'd better wake up and apologize.\"\n" +
-        "\"I Will Praise Any Man That Will Praise Me.\"";
+        String expectedOutput = """
+                "I Don't Believe In Failure. It Is Not Failure If You Enjoyed The Process."
+                
+                "Do not get elated at any victory, for all such victory is subject to the will of God."
+                "Wear gratitude like a cloak and it will feed every corner of your life."
+                "If you even dream of beating me you'd better wake up and apologize."
+                "I Will Praise Any Man That Will Praise Me.\"""";
+        assertEquals(expectedOutput, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void readsNumberOptionAndPrintsToStandardOutput() {
+        String[] args = new String[] { "-n", "src/test/resources/test2.txt" };
+        Javacat.main(args);
+        String expectedOutput = """
+                1 "I Don't Believe In Failure. It Is Not Failure If You Enjoyed The Process."
+                2
+                3 "Do not get elated at any victory, for all such victory is subject to the will of God."
+                4 "Wear gratitude like a cloak and it will feed every corner of your life."
+                5 "If you even dream of beating me you'd better wake up and apologize."
+                6 "I Will Praise Any Man That Will Praise Me.\"""";
         assertEquals(expectedOutput, outputStreamCaptor.toString().trim());
     }
 }
